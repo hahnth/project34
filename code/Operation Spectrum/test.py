2b4ca149887932b9f3ed5f4e63dabd2620f34084
Yannick Schubert <schuyann@student.ethz.ch>
2018-11-06 16:58:03 +0100

    Data AS-Oregon

--- code/Operation Spectrum/test.py
+++ code/Operation Spectrum/test.py
@@ -17,6 +17,8 @@ C = importMatrixFile(".\data\cities.txt", ',', ',')
 print("cities")
 obtainMaxEig(C, True, 2)
 
+plotFromGraph(C)
+
 D = importMatrixFile(".\data\windsurfers.txt", '\n', ' ')
 print("windsurfers")
 obtainMaxEig(D, True, 2)
@@ -25,13 +27,19 @@ E = importMatrixFile(".\data\guineaTribe.txt", '\n', ' ')
 print("guineaTribe")
 obtainMaxEig(E, True, 2)
 
-F = importEdgeListFile('.\data\\terrorist.txt', '\t')
-print("terrorist")
-obtainMaxEig(F, True, 2)
+#F = importEdgeListFile('.\data\\terrorist.txt', '\t')
+#print("terrorist")
+#obtainMaxEig(F, True, 2)
 
-plotFromGraph(C)
+G = importEdgeListFile(".\data\\asgraph\\asgraph.txt", ':')
+print('Oregon-AS graph')
+#Warning: Can take up to 10 min to plot the graphs and you don't see very much
+#plotFromGraph(G)
+#plotFromGraph(G)
+obtainMaxEig(G, True, 3)
+
 
 #Warning: Large file, takes up to 2 minutes
-#F = importEdgeListFile(".\data\hyves\edges.csv", ',')
-print("hyves")
-#obtainMaxEig(F, True, 3)
+#H = importEdgeListFile(".\data\hyves\edges.csv", ',')
+#print("hyves")
+#obtainMaxEig(H, True, 3)
